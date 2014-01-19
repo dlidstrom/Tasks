59391fc19a6e00d265d014b503bf5a8c56aa240f
Daniel Lidstrom <dlidstrom@gmail.com>
2014-01-19 20:52:41 +0100

    Implemented tests for TaskService

--- Tasks.Tests/run-jasmine.js
+++ Tasks.Tests/run-jasmine.js
@@ -1,4 +1,5 @@
-var system = require('system');
+var system = require('system'),
+  env = system.env;
 
 /**
  * Wait until the test condition is true or a timeout occurs. Useful for waiting
@@ -16,25 +17,24 @@ function waitFor(testFx, onReady, timeOutMillis) {
     var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3001, //< Default Max Timeout is 3s
         start = new Date().getTime(),
         condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+        interval = setInterval(function () {
+            if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
                 // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+                condition = (typeof (testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
             } else {
-                if(!condition) {
+                if (!condition) {
                     // If condition still not fulfilled (timeout but condition is 'false')
                     console.log("'waitFor()' timeout");
                     phantom.exit(1);
                 } else {
                     // Condition fulfilled (timeout and/or condition is 'true')
                     console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    typeof (onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
                     clearInterval(interval); //< Stop this interval
                 }
             }
         }, 100); //< repeat check every 100ms
 };
-
 
 if (system.args.length !== 2) {
     console.log('Usage: run-jasmine.js URL');
@@ -43,41 +43,124 @@ if (system.args.length !== 2) {
 
 var page = require('webpage').create();
 
+/**
+ *
+##teamcity[testSuiteStarted name='suite.name']
+##teamcity[testSuiteStarted name='nested.suite']
+##teamcity[testStarted name='package_or_namespace.ClassName.TestName']
+##teamcity[testFailed name='package_or_namespace.ClassName.TestName' message='The number should be 20000' details='expected:<20000> but was:<10000>']
+##teamcity[testFinished name='package_or_namespace.ClassName.TestName']
+##teamcity[testSuiteFinished name='nested.suite']
+##teamcity[testSuiteFinished name='suite.name']
+ */
 // Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function(msg) {
-    console.log(msg);
+page.onConsoleMessage = function (msg) {
+    var teamCityMessage = msg.indexOf('TEAMCITY_') === 0;
+    if (teamCityMessage) {
+        if (!env.hasOwnProperty('TEAMCITY_PROJECT_NAME')) return;
+        var separatorIndex = msg.indexOf(':');
+        var command = msg.substring(0, separatorIndex);
+        var data = JSON.parse(msg.substring(separatorIndex + 1));
+        switch (command) {
+            case 'TEAMCITY_TESTSTARTED': {
+                console.log("##teamcity[testStarted name='" + escape(data.name) + "']");
+                break;
+            }
+            case 'TEAMCITY_TESTFINISHED': {
+                console.log("##teamcity[testFinished name='" + escape(data.name) + "']");
+                break;
+            }
+            case 'TEAMCITY_SUITESTARTED': {
+                console.log("##teamcity[testSuiteStarted name='" + escape(data.suite) + "']");
+                break;
+            }
+            case 'TEAMCITY_SUITEFINISHED': {
+                console.log("##teamcity[testSuiteFinished name='" + escape(data.suite) + "']");
+                break;
+            }
+            case 'TEAMCITY_TESTFAILED': {
+                console.log("##teamcity[testFailed name='" + escape(data.name) + "' message='" + escape(data.message) + "']");
+                break;
+            }
+        }
+    }
+    else {
+        if (env.hasOwnProperty('TEAMCITY_PROJECT_NAME')) return;
+        console.log(msg);
+    }
+
+    function escape(message) {
+        while (message.indexOf("'") >= 0) message = message.replace("'", '"');
+        return message;
+    }
 };
 
-page.open(system.args[1], function(status){
+page.open(system.args[1], function (status) {
     if (status !== "success") {
         console.log("Unable to access network");
         phantom.exit();
     } else {
-        waitFor(function(){
-            return page.evaluate(function(){
+        waitFor(function () {
+            return page.evaluate(function () {
                 return document.body.querySelector('.symbolSummary .pending') === null
             });
-        }, function(){
-            var exitCode = page.evaluate(function(){
-                console.log('');
-                console.log(document.body.querySelector('.description').innerText);
-                var list = document.body.querySelectorAll('.results > #details > .specDetail.failed');
-                if (list && list.length > 0) {
-                  console.log('');
-                  console.log(list.length + ' test(s) FAILED:');
-                  for (i = 0; i < list.length; ++i) {
-                      var el = list[i],
-                          desc = el.querySelector('.description'),
-                          msg = el.querySelector('.resultMessage.fail');
-                      console.log('');
-                      console.log(desc.innerText);
-                      console.log(msg.innerText);
-                      console.log('');
-                  }
-                  return 1;
+        }, function () {
+            var exitCode = page.evaluate(function () {
+                var currentSuite;
+                var successList = document.body.querySelectorAll('.results > .summary .specSummary.passed');
+                var suites = {};
+                if (successList && successList.length > 0) {
+                    for (var i = 0; i < successList.length; ++i) {
+                        var el = successList[i],
+                            name = el.children[0].title,
+                            suite = name.substring(0, name.indexOf(' '));
+
+                        suites[suite] = suites[suite] || [];
+                        suites[suite].push({ status: 'success', name: name });
+                    }
+                }
+
+                var failedList = document.body.querySelectorAll('.results > #details > .specDetail.failed');
+                if (failedList && failedList.length > 0) {
+                    console.log('');
+                    console.log(failedList.length + ' test(s) FAILED:');
+                    for (var i = 0; i < failedList.length; ++i) {
+                        var el = failedList[i],
+                            name = el.querySelector('.description').innerText,
+                            msg = el.querySelector('.resultMessage.fail').innerText,
+                            suite = name.substring(0, name.indexOf(' '));
+
+                        // remove trailing period
+                        name = name.substring(0, name.length - 1);
+
+                        suites[suite] = suites[suite] || [];
+                        suites[suite].push({ suite: suite, status: 'failed', name: name, message: name + ': ' + msg });
+                    }
+
+                    for (var suite in suites) {
+                        var tests = suites[suite];
+                        console.log('TEAMCITY_SUITESTARTED:' + JSON.stringify({ suite: suite }));
+                        for (var i in tests) {
+                            var test = tests[i];
+                            console.log('TEAMCITY_TESTSTARTED:' + JSON.stringify({ name: test.name }));
+                            if (test.status === 'success') {
+                            }
+                            else if (test.status === 'failed') {
+                                console.log('TEAMCITY_TESTFAILED:' + JSON.stringify({ name: test.name, message: test.message }));
+                                console.log('');
+                                console.log(test.suite);
+                                console.log(test.message);
+                            }
+
+                            console.log('TEAMCITY_TESTFINISHED:' + JSON.stringify({ name: test.name }));
+                        }
+
+                        console.log('TEAMCITY_SUITEFINISHED:' + JSON.stringify({ suite: suite }));
+                    }
+                    return 1;
                 } else {
-                  console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
-                  return 0;
+                    console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
+                    return 0;
                 }
             });
             phantom.exit(exitCode);
---
+++ Tasks.Tests/spec/taskservice.spec.js
@@ -0,0 +1,47 @@
+﻿describe('TaskService', function () {
+    'use strict';
+
+    var httpMock;
+    var taskService;
+
+    beforeEach(function () {
+        module('App');
+    });
+
+    beforeEach(inject(function ($httpBackend) {
+        httpMock = $httpBackend;
+    }));
+
+    beforeEach(inject(['TaskService', function (service) {
+        taskService = service;
+    }]));
+
+    it('should inject task service', function () {
+        expect(taskService).toBeDefined();
+    });
+
+    describe('api', function () {
+        afterEach(function () {
+            httpMock.flush();
+            httpMock.verifyNoOutstandingExpectation();
+            httpMock.verifyNoOutstandingRequest();
+        });
+
+        it('should create task', function () {
+            httpMock.expectPOST('api/task', { responsible: 'Somebody', task: 'Some task' }).respond(201);
+            taskService.create({ responsible: 'Somebody', task: 'Some task' });
+        });
+
+        it('should delete task', function () {
+            httpMock.expectDELETE('api/task?responsible=Somebody&task=Some+task').respond(201);
+            taskService.delete({ responsible: 'Somebody', task: 'Some task' });
+        });
+
+        describe('changeDone', function () {
+            it('should put new task', function () {
+                httpMock.expectPUT('api/task', { responsible: 'Somebody', task: 'Some task', done: true }).respond(201);
+                taskService.changeDone({ responsible: 'Somebody', task: 'Some task', done: false });
+            });
+        });
+    });
+});
