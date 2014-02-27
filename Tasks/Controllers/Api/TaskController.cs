b4a907ca83b0ed5faed65f51708cebc75a642c1c
Daniel Lidström <dlidstrom@gmail.com>
2014-02-27 09:04:05 +0100

    Update TaskController.cs

--- Tasks/Controllers/Api/TaskController.cs
+++ Tasks/Controllers/Api/TaskController.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -14,22 +14,22 @@ namespace Tasks.Controllers.Api
             if (await ExecuteQueryAsync(new GetTaskQuery(request.Responsible, request.Task)) != null)
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Task already exists");
 
-            ExecuteCommand(new AddTaskCommand(request.Responsible, request.Task));
+            await ExecuteCommand(new AddTaskCommand(request.Responsible, request.Task));
             var task = await ExecuteQueryAsync(new GetTaskQuery(request.Responsible, request.Task));
             return Request.CreateResponse(
                 HttpStatusCode.Created,
                 task);
         }
 
-        public HttpResponseMessage Put(TaskRequest request)
+        public async Task<HttpResponseMessage> Put(TaskRequest request)
         {
-            ExecuteCommand(new ChangeTaskCommand(request.Responsible, request.Task, request.Done));
+            await ExecuteCommand(new ChangeTaskCommand(request.Responsible, request.Task, request.Done));
             return Request.CreateResponse(HttpStatusCode.Accepted);
         }
 
-        public HttpResponseMessage Delete([FromUri] TaskRequest request)
+        public async Task<HttpResponseMessage> Delete([FromUri] TaskRequest request)
         {
-            ExecuteCommand(new DeleteTaskCommand(request.Responsible, request.Task));
+            await ExecuteCommand(new DeleteTaskCommand(request.Responsible, request.Task));
             return Request.CreateResponse(HttpStatusCode.Accepted);
         }
 
